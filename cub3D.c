69d2ccae873a65b9031fb657972228b4a9aafa27
Anas Khaliss <akhaliss@e1r4p15.1337.ma>
2023-10-31 17:36:52 +0100

    main modified

--- cub3D.c
+++ cub3D.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
+/*   By: akhaliss <akhaliss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 13:43:18 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/10/31 16:48:45 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/10/31 17:34:26 by akhaliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,22 @@ int	main(int ac, char **av)
 	else
 	{
 		if (cub(av[1], g))
-			printf("done\n");
+		{		
+			g->mlx = mlx_init();
+			init_game(g, p);
+			init_player(g);
+			main_draws(g);
+			mlx_hook(g->win, X_EVENT_KEY_PRESS, 0, &key_press, g);
+			mlx_hook(g->win, 6, 1L << 6, mouse_move, g);
+			mlx_hook(g->win, X_EVENT_KEY_EXIT, 0, &escape_game, g);
+			mlx_loop(g->mlx);
+		}
+		// printf("done\n");
 		else
 		{
 			write(2, "invalid map\n", 12);
 			return (0);
 		}
 	}
-	g->mlx = mlx_init();
-	init_game(g, p);
-	init_player(g);
-	main_draws(g);
-	mlx_hook(g->win, X_EVENT_KEY_PRESS, 0, &key_press, g);
-	mlx_hook(g->win, 6, 1L << 6, mouse_move, g);
-	mlx_hook(g->win, X_EVENT_KEY_EXIT, 0, &escape_game, g);
-	mlx_loop(g->mlx);
 	return (0);
 }
