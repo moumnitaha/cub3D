f85698c3a9c1d69d6a4a535b45171b562ba1ef51
Anas Khaliss <akhaliss@e3r6p3.1337.ma>
2023-11-14 16:52:57 +0100

    heap problem solved

--- draw_rays.c
+++ draw_rays.c
@@ -6,7 +6,7 @@
 /*   By: akhaliss <akhaliss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 18:23:32 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/11/14 15:44:09 by akhaliss         ###   ########.fr       */
+/*   Updated: 2023/11/14 16:52:07 by akhaliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,8 @@ void	handle_ray_dis(t_ray *ray)
 		ray->dist = ray->h_hit_dis;
 	else if (ray->is_vc_hit)
 		ray->dist = ray->v_hit_dis;
+	else
+		ray->dist = INFINITY;
 }
 
 void	render_rays(t_game *g)
